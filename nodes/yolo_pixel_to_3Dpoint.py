380ef1c83ce8672bb337b575ed18c440b5aad373
Mohamed Fazil <mohamedfazilsulaiman@gmail.com>
2021-10-17 12:15:31 -0400

    3D Pose Estimation Working

--- nodes/yolo_pixel_to_3Dpoint.py
+++ nodes/yolo_pixel_to_3Dpoint.py
@@ -11,17 +11,19 @@ from rospy.numpy_msg import numpy_msg
 import time
 import pyrealsense2
 from cv_bridge import CvBridge, CvBridgeError
+import cv2
+global obj,loc,cnt,depth_img,pose,rect
 
-global obj,loc,cnt,depth_img,pose
+obj = rospy.get_param('class','bottle')
 
-pose = [None,None,None]
-depth_img = None
-obj = 'bottle'
+rect = None
+pose = [0,0,0]
+depth_img = np.zeros((480,640))
 loc = [None,None]
 cnt = 0
 
 def callback(msg):
-    global obj,loc,cnt,pose,depth_img
+    global obj,loc,cnt,pose,depth_img,rect
     bounding_boxes = msg.bounding_boxes
     if(cnt>0):
         for box in bounding_boxes:
@@ -30,12 +32,14 @@ def callback(msg):
                 ymin = box.ymin
                 xmax = box.xmax
                 ymax = box.ymax
-                x = (xmax - xmin)/2
-                y = (ymax - ymin)/2
-                loc = [xmin,ymin]
+                rect = [xmin,ymin,xmax,ymax]
+                x = rect[2] - (rect[2]-rect[0])/2
+                y = rect[3] - (rect[3]-rect[1])/2
+                loc = [x,y]
             # else:
             #     loc = [None,None]
-    print(obj,' loc = ',loc,' 3dPose=',pose,depth_img[loc[0]][loc[1]])
+    print(obj,' loc = ',loc,' 3dPose=',pose)
+    
 
 
 def object_count(msg):
@@ -64,14 +68,24 @@ def cam_info(msg):
 
 def vis_callback(msg):
     global depth_img
-    # im = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+    track_im = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
     bridge = CvBridge()
     try:
         im = bridge.imgmsg_to_cv2(msg, "passthrough")
     except CvBridgeError as e:
       print(e)
     depth_img = im
+    show_image(im)
 
+def show_image(img):
+    global rect,pose, obj
+    img = cv2.normalize(img, dst=None, alpha=0, beta=65535, norm_type=cv2.NORM_MINMAX)
+    img = cv2.rectangle(img, (rect[0],rect[1]), (rect[2],rect[3]), (255, 0, 0), 2)
+    text = obj+' '+str(np.around(pose,decimals=3))
+    image = cv2.putText(img, text, (rect[2] - (rect[2]-rect[0])/2,rect[3] - (rect[3]-rect[1])/2), cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.8, (255, 0, 0), 1, cv2.LINE_AA)
+    cv2.imshow("Pose Track (mm)", img)
+    cv2.waitKey(3)
 
 if __name__=='__main__':
     rospy.init_node('yolo_handler')
@@ -79,6 +93,14 @@ if __name__=='__main__':
     rospy.Subscriber("/darknet_ros/found_object", ObjectCount, object_count) 
     rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, vis_callback) 
     rospy.Subscriber("/camera/color/camera_info", CameraInfo, cam_info) 
-    
+    br = tf.TransformBroadcaster()
     rate = rospy.Rate(10.0)
-    rospy.spin()
+    while not rospy.is_shutdown():
+        pose_tf = np.array(pose)
+        br.sendTransform((pose_tf[2]/1000, -pose_tf[0]/1000, pose_tf[1]/1000),
+                            (0.0, 0.0, 0.0, 1.0),
+                            rospy.Time.now(),
+                            "object",
+                            "camera_link")
+    rate.sleep()
+    # rospy.spin()
