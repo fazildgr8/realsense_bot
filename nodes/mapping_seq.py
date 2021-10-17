9db347445c950f7ba265605dd5b932d4a931c64f
Mohamed Fazil <mohamedfazilsulaiman@gmail.com>
2021-10-16 21:38:33 -0400

    Robot Mapping Stack Done

--- nodes/mapping_seq.py
+++ nodes/mapping_seq.py
@@ -1,73 +1,78 @@
 #!/usr/bin/python
+from numpy.lib.function_base import angle
 import roslib
 import rospy
 import tf
 import math
 import random
 from std_msgs.msg import Int16MultiArray
+from sensor_msgs.msg import JointState
 import numpy as np
 import time
 
 
-def look_around_ccw(servo,m,u=90):
-    cmd = Int16MultiArray()
-    cmd.data = [0,m,u]
+def look_around_ccw(joint_control,m=0,u=0):
+    cmd = JointState()
+    cmd.position = [0,m,u]
+    cmd.name = ['Joint_1','Joint_2','Joint_3']
     delay = 0.1
     time.sleep(0.8)
-    servo.publish(cmd)
-
-    for i in range(0,180):
-        cmd.data = [i,m,u]
-        servo.publish(cmd)
+    joint_control.publish(cmd)
+    angles = np.linspace(-1.57,1.57,180)
+    for i in range(len(angles)):
+        cmd.position = [angles[i],m,u]
+        joint_control.publish(cmd)
         time.sleep(delay)
 
 
-def look_around_cw(servo,m,u=90):
-    cmd = Int16MultiArray()
-    cmd.data = [0,m,u]
+def look_around_cw(joint_control,m=0,u=0):
+    cmd = JointState()
+    cmd.name = ['Joint_1','Joint_2','Joint_3']
+    cmd.position = [0,m,u]
     delay = 0.1
     time.sleep(0.8)
-    servo.publish(cmd)
+    joint_control.publish(cmd)
+    angles = np.linspace(-1.57,1.57,180)
+    angles = np.flip(angles,0)
 
-    for i in range(0,180):
-        cmd.data = [180-i,m,u]
-        servo.publish(cmd)
+    for i in range(len(angles)):
+        cmd.position = [angles[i],m,u]
+        joint_control.publish(cmd)
         time.sleep(delay)
 
-def look_around_m(servo):
-    m_range = np.linspace(25,125,10)
-    u = 90
+def map_sequence(joint_control):
+    m_range = np.linspace(1.4,-1.4,10)
     for i in range(len(m_range)):
-        # if m_range[i]<100:
-        if i%2 == 0:
-            look_around_ccw(servo,m_range[i])
+        if i%2==0:
+            look_around_ccw(joint_control,m_range[i])
+            look_around_cw(joint_control,m_range[i])
         else:
-            look_around_cw(servo,m_range[i])
-        # else:
-        #     if i%2 == 0:
-        #         look_around_ccw(servo,m_range[i],m_range[i])
-        #     else:
-        #         look_around_cw(servo,m_range[i],180-m_range[i])
+            look_around_ccw(joint_control,m_range[i])
+            look_around_cw(joint_control,m_range[i])
 
-
-def robot_home(servo):
-    cmd = Int16MultiArray()
-    cmd.data = [90,90,90] # Robot Home POSITION
-    servo.publish(cmd)
+def robot_home(joint_control):
+    cmd = JointState()
+    cmd.name = ['Joint_1','Joint_2','Joint_3']
+    cmd.position = [0,0,0] # Robot Home POSITION
+    joint_control.publish(cmd)
 
 
 if __name__=='__main__':
     rospy.init_node('look_around')
 
     rate = rospy.Rate(10.0)
-    cmd = Int16MultiArray()
-    servo = rospy.Publisher('/servo', Int16MultiArray,queue_size=10) # Publish Command
-    cmd.data = [90,90,90] # Robot Home POSITION
-    servo.publish(cmd)
-    # look_around(servo,90)
-    look_around_m(servo)
-    robot_home(servo)
+    cmd = JointState()
+    
+    joint_control = rospy.Publisher('/joint_states_ct',JointState,queue_size=10) # Publish Joint States
+    cmd.name = ['Joint_1','Joint_2','Joint_3']
+    cmd.position = [0,0,0] # Robot Home POSITION
+    # time.sleep(5)
+    joint_control.publish(cmd)
+    map_sequence(joint_control)
+    robot_home(joint_control)
     # while not rospy.is_shutdown():
-    #     # cmd.data = [90,90,90]
-    #     # servo.publish(cmd)
-    #     rate.sleep()
+        # cmd.data = [90,90,90]
+        # servo.publish(cmd)
+        # joint_control.publish(cmd)
+        # rate.sleep()
+    # rospy.spin()
