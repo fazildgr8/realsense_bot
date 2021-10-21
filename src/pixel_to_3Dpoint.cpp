e53d8e37e7d8d57b023929ff79b448713e8b871d
Mohamed Fazil <mohamedfazilsulaiman@gmail.com>
2021-10-21 03:48:43 -0400

    CPP Version

--- src/pixel_to_3Dpoint.cpp
+++ src/pixel_to_3Dpoint.cpp
@@ -1,26 +1,33 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <vector>
+#include <string>
+// #include <darknet_ros_msgs/BoundingBox.h>
+// #include <darknet_ros_msgs/BoundingBoxes.h>
+// #include <darknet_ros_msgs/ObjectCount.h>
 
-float pose[3];
 
-void object__pose_broadcaster(string object_name){
+void object_pose_broadcaster(float pose[3],std::string object_name){
   static tf::TransformBroadcaster br;
   tf::Transform transform;
-  transform.setOrigin(tf::Vector3(1,2,3));
-  // transform.setRotation();
+  transform.setOrigin(tf::Vector3(pose[0],pose[1],pose[2]));
+  tf::Quaternion q;
+  q.setRPY(0,0,0);
+  transform.setRotation(q);
   br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", object_name));
 }
 
+
 int main(int argc, char** argv){
-  ros::init(argc, argv, "my_tf_broadcaster");
+  ros::init(argc, argv, "objects_broadcaster");
   ros::NodeHandle node;
-  while (1)
+
+  float pose[3] = {1,2,3};
+
+  while (ros::ok())
   {
-    // pose = {1,2,3};
-    object__pose_broadcaster();
+    object_pose_broadcaster(pose,"object");
   }
-  
   ros::spin();
   return 0;
 };
